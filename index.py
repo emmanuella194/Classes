c3077e2474bb9b9a38ceb98638450be8cc14a789
Emmanuella Orioma <oriomaemmanuella@gmail.com>
2023-01-10 00:22:22 +0100

    Update index.py

--- index.py
+++ index.py
@@ -39,30 +39,7 @@
   # print(row)
 
 #using the class .collect and .extract
-import csv
 
-class collect:
-    def __init__(self, row):
-        self.state = row['state']
-        self.position = row['position']
-        self.age = row['age']
-
-class DataCollection:
-    def __init__(self):
-        self.data_list = []
-    
-    def collect(self, file_path):
-        with open(file_path, 'r') as f:
-            reader = csv.DictReader(f)
-            self.data_list = [Data(row) for row in reader]
-    
-    def extract(self):
-        for data in self.data_list:
-            print(f'state: {data.state}, position: {data.position}, age: {data.age}')
-
-data_collection = DataCollection()
-data_collection.collect('data.csv')
-data_collection.extract()
 #This code defines a Data class that represents a single row of data in the CSV file,
  #with attributes for the name, age, and country fields. 
  #It also defines a DataCollection class that has a collect method to accept a file path
@@ -73,26 +50,18 @@ data_collection.extract()
 
 import csv
 
-class collect:
-    def __init__(self, row):
-        self.state = row['state']
-        self.position = row['position']
-        self.age = row['age']
-
-class DataCollection:
+class csv_parser:
     def __init__(self):
         self.data_list = []
     
     def collect(self, file_path):
         with open(file_path, 'r') as f:
-            reader = csv.DictReader(f)
-            self.data_list = [row for row in reader]
+            self.data_list = [row for row in csv.DictReader(f)]
     
     def extract(self):
         for i in self.data_list:
-            for k, v in i.items():
-                print(k, v)
+            [print(k, v) for k, v in i.items()]
 
-data_collection = DataCollection()
+data_collection = csv_parser()
 data_collection.collect('file.csv')
 data_collection.extract()
